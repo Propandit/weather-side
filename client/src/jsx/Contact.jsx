d19ad31c12207dff21b11da4eceaa5f81aee8a05
propandit <panditanshul4444@gmail.com>
2025-04-12 15:34:41 +0530

    Deleted folder_name

--- client/src/jsx/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Contact (){
-return <address>
-		Please connect with me at <a href='mailto:kunaalgupta@hotmail.com'>kunaalgupta@hotmail.com</a> 
-		</address>
-}
-
-export default Contact;
